b0d0aada85fb0c39be9d24679a232803d2f66ed1
Fabricio Escalante <ShortEmperor194@gmail.com>
2022-02-04 17:56:06 -0600

    Finishing the code for the second practice

---
+++ Practica1.2/typeOfTtriangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double sideA, sideB, sideC;
+    cout << "Type in the first side of the triangle: ";
+    cin >> sideA;
+    cout << "Type in the second side of the triangle: ";
+    cin >> sideB;
+    cout << "Type in the third side of the triangle: ";
+    cin >> sideC;
+
+    if(sideA == sideB and sideB == sideC){
+        cout << "The triangle is equilateral!";
+    }else if(sideA == sideB || sideB == sideC || sideA == sideC){
+        cout << "The triangle is Isosceles";
+    }else{
+        cout << "The triangle is Scalane";
+    }
+    return 0;
+}
